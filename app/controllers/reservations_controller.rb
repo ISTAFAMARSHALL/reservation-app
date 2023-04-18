73d3f49c147d791571c6e9014ff36cd586f44683
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:28:34 -0400

    added full CRUD to reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,36 @@
 class ReservationsController < ApplicationController
+
+    def index
+        reservations = Reservation.all 
+        render json: reservations, status: :ok
+    end
+
+    def show
+        reservation = Reservation.find(params[:id])
+        render json: reservation, status: :ok
+    end
+
+    def create
+        reservation = Reservation.create!(reservation_params)
+        render json reservation, status: :created
+    end
+
+    def update
+        reservation = Reservation.find(params[:id])
+        updated_reservation = reservation.update(reservation_params)
+        render json: updated_reservation, status: :accepted
+    end
+
+    def destroy
+        reservation = Reservation.find(params[:id])
+        reservation.destroy
+        head :no_content
+    end
+
+    private
+
+    def reservation_params
+        params.permit(:name, :number_of_guests, :time, :patron_id, :restaurant_id )
+    end
+    
 end
