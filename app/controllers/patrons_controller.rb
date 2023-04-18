57f1faaa51a92856d9f4d1c2db78e4a75b8b70fa
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:27:52 -0400

    added full CRUD to patrons controller

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -1,2 +1,36 @@
 class PatronsController < ApplicationController
+
+    def index
+        patrons = Patron.all 
+        render json: patrons, status: :ok
+    end
+
+    def show
+        patron = Patron.find(params[:id])
+        render json: patron, status: :ok
+    end
+
+    def create
+        patron = Patron.create!(patron_params)
+        render json patron, status: :created
+    end
+
+    def update
+        patron = Patron.find(params[:id])
+        updated_patron = patron.update(patron_params)
+        render json: updated_patron, status: :accepted
+    end
+
+    def destroy
+        patron = Patron.find(params[:id])
+        patron.destroy
+        head :no_content
+    end
+
+    private
+
+    def patron_params
+        params.permit(:name,:phone_number,:email_address)
+    end
+    
 end
