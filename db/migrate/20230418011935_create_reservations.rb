12f95065c9909bd4628d2b1300015fdebce49edb
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:22:58 -0400

    added error handlers to application controller

--- db/migrate/20230418011935_create_reservations.rb
+++ db/migrate/20230418011935_create_reservations.rb
@@ -2,12 +2,10 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.string :name
-      t.string :number_of_guests
+      t.integer :number_of_guests
       t.string :time
-      t.string :belongs_to
-      t.string :patron
-      t.string :belongs_to
-      t.string :restaurant
+      t.integer :patron_id
+      t.integer :restaurant_id
 
       t.timestamps
     end
