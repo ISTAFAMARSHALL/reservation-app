afb88102d8bf2dc0fc13ba6c14897dd6f0c6060b
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:29:55 -0400

    added full CRUD to restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+
+    def index
+        restaurants = Restaurant.all 
+        render json: restaurants, status: :ok
+    end
+
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant, status: :ok
+    end
+
+    def create
+        restaurant = Restaurant.create!(restaurant_params)
+        render json restaurant, status: :created
+    end
+
+    def update
+        restaurant = Restaurant.find(params[:id])
+        updated_restaurant = restaurant.update(restaurant_params)
+        render json: updated_restaurant, status: :accepted
+    end
+
+    def destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        head :no_content
+    end
+
+    private
+
+    def restaurant_params
+        params.permit(:name, :cuisine, :description)
+    end
+    
 end
