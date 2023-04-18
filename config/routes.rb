8178bfdb64a317c3e92df8e73f06bb172f0dd1cd
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:38:30 -0400

    added resource routes for patrons restaurants and reservations

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :patrons
+  resources :reservations
+  resources :restaurants
+  
 end
