7ae18e814f4f526553abac27e8633a26ba24eda9
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:31:50 -0400

    added validations and active record associations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ApplicationRecord
+    validates :name, :number_of_guests, :time, :patron_id, :restaurant_id, presence: true
+    
+    belongs_to :patron
+    belongs_to :restaurant
 end
