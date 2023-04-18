a94cac1ec3f2a97c5bd3479f5c371ea63dda6d21
istafamarshall <istafa.a.marshall@gmail.com>
2023-04-17 21:31:10 -0400

    added validations and active record associations to patron model

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -1,2 +1,7 @@
 class Patron < ApplicationRecord
+    validates :name, :email_address, presence: true
+    # validates :phone_number, length: { is: 14 }
+
+    has_many :reservations
+    has_many :restaurants, through: :reservations
 end
