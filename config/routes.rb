1120a30c39bb496a72ab3b57bc3acf16088f2a75
istafamarshall <istafa.a.marshall@gmail.com>
2023-05-04 11:22:05 -0400

    updated route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  
-  namespace :api do
+
+ 
 
     root "patrons#show"
 
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
 
-  end
+ 
 
   
   
