fa7373a79ab82a78e8457c681aebc8b6cab6f8f6
istafamarshall <istafa.a.marshall@gmail.com>
2024-07-02 23:17:42 -0400

    prep for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,16 +9,28 @@ import { UserProvider } from "./context/user";
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Router>
+//     <React.StrictMode>
+//       <UserProvider >
+//         <App />
+//       </UserProvider>
+//     </React.StrictMode>
+//   </Router>
+// );
+
+ReactDOM.render(
   <Router>
-    <React.StrictMode>
-      <UserProvider >
-        <App />
-      </UserProvider>
-    </React.StrictMode>
-  </Router>
+  <React.StrictMode>
+  <UserProvider >
+    <App />
+    </UserProvider>
+  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
